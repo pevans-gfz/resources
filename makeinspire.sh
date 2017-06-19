175b5b9b53c0fed633a778eff903cca5b7d540a0
Peter Evans <pevans@gfz-potsdam.de>
2017-06-19 12:51:37 +0200

    Try converting all available DataCite XML files

---
+++ makeinspire.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Convert all DataCite XML files to ISO19139.
+
+# Requires xsltproc and a suitable XSLT.
+# The XSLT was developed by ....
+# and is used under the Apache License, v 2.0, January 2004.
+#
+
+set -u
+
+inputdir=datacite
+outputdir=iso19139
+
+[ ! -d "$inputdir" ] && echo "No datacite directory ${inputdir}" && exit 1
+[ ! -d "$outputdir" ] && echo "No output directory ${outputdir}" && exit 1
+
+for x in $(cd ${inputdir}; ls *DataCite3.xml) ; do
+	echo "File $x"
+	outfile=${outputdir}/$(basename $x DataCite3.xml)19139.xml
+	#echo $outfile
+	xsltproc dataciteToISO19139v3.xslt ${inputdir}/$x >$outfile
+done
